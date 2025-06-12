54e52a0a403849cb1c59ae2f412a81435e7fa980
Michael Tester <trainee85.glophics@gmail.com>
2025-06-12 08:57:28 +0800

    removed electron due to file storage

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
 const { Builder } = require("selenium-webdriver");
 const chromedriverPath = require('chromedriver').path;
 const chrome = require('selenium-webdriver/chrome');
@@ -9,40 +7,9 @@ const { uploadFile } = require("./components/UploadFile");
 
 const SINGAPRINTING_URL = "https://www.singaprinting.com/";
 
-let mainWindow;
-
-function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 500,
-        height: 400,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-    mainWindow.loadFile('index.html');
-}
-
-function sendLog(message) {
-    if (mainWindow) {
-        mainWindow.webContents.send('automation-log', message);
-    }
-}
-
-app.whenReady().then(createWindow);
-
-ipcMain.on('start-automation', async (event) => {
-    sendLog("Starting automation...");
-    
-    await runAutomation();
-   
-    event.reply('automation-finished', 'Automation completed!');
-    
-});
-
 async function runAutomation() {
     // Explicitly set the ChromeDriver path
-  const service = new chrome.ServiceBuilder(chromedriverPath);
+    const service = new chrome.ServiceBuilder(chromedriverPath);
     const options = new chrome.Options();
 
     let driver = await new Builder()
@@ -52,23 +19,21 @@ async function runAutomation() {
         .build();
 
     try {
-        sendLog("Opening website...");
+        console.log("Opening website...");
         await driver.get(SINGAPRINTING_URL);
         await driver.manage().window().maximize();
-        sendLog("Logging in...");
+        console.log("Logging in...");
         await login(driver, "trainee85.glophics@gmail.com", "ephemeral123!?");
-        sendLog("Selecting Die Cut options...");
+        console.log("Selecting Die Cut options...");
         await selectDieCutOptions(driver, true, "Die Cut", "Square", "Gloss", "Normal", 100);
-        sendLog("Uploading file...");
+        console.log("Uploading file...");
         await uploadFile(driver);
-        sendLog("All steps completed.");
+        console.log("All steps completed.");
     } catch (error) {
-        sendLog("Test failed: " + error);
+        console.log("Test failed: " + error);
     } finally {
         // await driver.quit();
     }
 }
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-});
+runAutomation();
